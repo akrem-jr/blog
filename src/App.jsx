5117289c77b13010463cb45248656e281033e81e
akrem0-jr <abdulbasetadem@gmail.com>
2024-12-07 16:27:51 +0300

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">Hello World</div>
+    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+      <Navbar />
+
+      Hello World
+
+
+
+
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       {/* LOGO */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="Lama Logo" w={32} h={32} />
-        <span>lamalog</span>
+        <span>Akrem Blog</span>
       </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
@@ -24,33 +24,29 @@ const Navbar = () => {
           {/* {open ? "X" : "☰"} */}
           <div className="flex flex-col gap-[5.4px]">
             <div
-              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
-                open && "rotate-45"
-              }`}
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${open && "rotate-45"
+                }`}
             ></div>
             <div
-              className={`h-[3px] rounded-md w-6 bg-black transition-all ease-in-out ${
-                open && "opacity-0"
-              }`}
+              className={`h-[3px] rounded-md w-6 bg-black transition-all ease-in-out ${open && "opacity-0"
+                }`}
             ></div>
             <div
-              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
-                open && "-rotate-45"
-              }`}
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${open && "-rotate-45"
+                }`}
             ></div>
           </div>
         </div>
         {/* MOBILE LINK LIST */}
         <div
-          className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
-            open ? "-right-0" : "-right-[100%]"
-          }`}
+          className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"
+            }`}
         >
-          <Link to="/" onClick={()=>setOpen(false)}>Home</Link>
-          <Link to="/posts?sort=trending" onClick={()=>setOpen(false)}>Trending</Link>
-          <Link to="/posts?sort=popular" onClick={()=>setOpen(false)}>Most Popular</Link>
-          <Link to="/" onClick={()=>setOpen(false)}>About</Link>
-          <Link to="/login" onClick={()=>setOpen(false)}>
+          <Link to="/" onClick={() => setOpen(false)}>Home</Link>
+          <Link to="/posts?sort=trending" onClick={() => setOpen(false)}>Trending</Link>
+          <Link to="/posts?sort=popular" onClick={() => setOpen(false)}>Most Popular</Link>
+          <Link to="/" onClick={() => setOpen(false)}>About</Link>
+          <Link to="/login" onClick={() => setOpen(false)}>
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login 👋
             </button>
