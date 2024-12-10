07051a8919941ea03487966bc6ecaae5ede4e1ef
akrem0-jr <abdulbasetadem@gmail.com>
2024-12-10 09:18:31 +0300

    something added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Image from "./Image";
 import { Link } from "react-router-dom";
-import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+// import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -11,7 +11,7 @@ const Navbar = () => {
       {/* LOGO */}
       <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="Lama Logo" w={32} h={32} />
-        <span>Akrem Blog</span>
+        <span>lamalog</span>
       </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
@@ -59,16 +59,16 @@ const Navbar = () => {
         <Link to="/posts?sort=trending">Trending</Link>
         <Link to="/posts?sort=popular">Most Popular</Link>
         <Link to="/">About</Link>
-        <SignedOut>
-          <Link to="/login">
-            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
-              Login 👋
-            </button>
-          </Link>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        {/* <SignedOut> */}
+        <Link to="/login">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login 👋
+          </button>
+        </Link>
+        {/* </SignedOut> */}
+        {/* <SignedIn> */}
+        {/* <UserButton /> */}
+        {/* </SignedIn> */}
       </div>
     </div>
   );
